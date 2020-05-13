d1d4dc436079434c3a39f0687b40a2d1fd48dac1
Sergey <sukyans@gmail.com>
2020-05-12 19:50:17 -0600

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,13 +4,13 @@ import '../styles/Footer.css'
 export default function Footer() {
   return (
     <div className="footer">
-      <a href="http://www.diamondway.org/ny/" target="_new">
-        DWBC NYC
+      <a href="http://www.diamondway.org/sf/" target="_new">
+        DWBC SF
       </a>{' '}
       •{' '}
-      <a href="http://www.diamondway.org/manhattan-project/" target="_new">
-        The Manhattan Project
-      </a>{' '}
+
+        Mantra Collection
+      
       • 2020
     </div>
   )
